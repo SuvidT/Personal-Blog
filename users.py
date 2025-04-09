e7aea7f9ef51130c2138e4931a43ceb8245579ff
Suvid Thungathurti <suvidthungathurti@Suvids-MacBook-Air.local>
2025-04-09 10:43:44 -0400

    added functions to work on

---
+++ users.py
@@ -0,0 +1,16 @@
+# ------------------------
+# CONSTANTS
+# ------------------------
+json_file = 'metadata/users.json'
+
+# ------------------------
+# FUNCTINOS
+# ------------------------
+def user_exists(username, password):
+    pass
+
+def make_user(username, password):
+    pass
+
+def make_admin(username):
+    pass
