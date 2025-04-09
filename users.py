f281940ee1a23f1a33d8f2a3d954f7e877cdae49
Suvid Thungathurti <suvidthungathurti@Suvids-MacBook-Air.local>
2025-04-09 12:39:27 -0400

    starting user stuff

--- users.py
+++ users.py
@@ -1,16 +1,23 @@
 # ------------------------
 # CONSTANTS
 # ------------------------
-json_file = 'metadata/users.json'
+json_user_files = 'metadata/users.json'
 
 # ------------------------
 # FUNCTINOS
 # ------------------------
 def user_exists(username, password):
-    pass
+    
 
 def make_user(username, password):
     pass
 
 def make_admin(username):
     pass
+
+def delete_user(username, password):
+    pass
+
+# ------------------------
+# TESTING
+# ------------------------
